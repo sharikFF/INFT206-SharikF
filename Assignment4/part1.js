8e21f2e71dee416999e865984c086aa4f2e5b813
sharikF <97528431+sharikF@users.noreply.github.com>
2026-04-09 23:41:44 -0400

    inital set up as well as first part with assigning constants making array and adding story cosntants

---
+++ Assignment4/part1.js
@@ -0,0 +1,57 @@
+// Complete variable definitions and random functions
+
+const customName = document.getElementById("custom-name");
+const generateBtn = document.querySelector(".generate");
+const story = document.querySelector(".story");
+
+function randomValueFromArray(array) {
+  const random = Math.floor(Math.random() * array.length);
+  return array[random];
+}
+
+// Raw text strings
+const characters=["Willy the Goblin","Big Daddy","Father Christmas"];
+const places=["the soup kitchen","Disneyland","the White House"];
+ const events=["spontaneously combusted","melted into a puddle on the sidewalk","turned into a slug and slithered away"];
+// Willy the Goblin
+// Big Daddy
+// Father Christmas
+
+// the soup kitchen
+// Disneyland
+// the White House
+
+// spontaneously combusted
+// melted into a puddle on the sidewalk
+// turned into a slug and slithered away
+
+// Partial return random string function
+
+function returnRandomStoryString() {
+   const randomCharacter=randomValueFromArray(characters);
+   const randomPlace=randomValueFromArray(places);
+   const randomEvent=randomValueFromArray(events);
+    storyText=`It was 94 Fahrenheit outside, so ${randomCharacter} went for a walk. When they got to ${randomPlace}, they stared in horror for a few moments, then ${randomEvent}. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.`
+  // It was 94 Fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+
+  return storyText;
+}
+
+// Event listener and partial generate function definition
+
+generateBtn.addEventListener("click", generateStory);
+
+function generateStory() {
+  if (customName.value !== "") {
+    const name = customName.value;
+  }
+
+  if (document.getElementById("uk").checked) {
+    const weight = Math.round(300);
+    const temperature = Math.round(94);
+  }
+
+  // TODO: replace "" with the correct expression
+  story.textContent = "";
+  story.style.visibility = "visible";
+}
