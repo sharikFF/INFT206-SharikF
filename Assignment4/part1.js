3d678131b58acce6c71a8ecbe707f49a4df7ea18
sharikF <97528431+sharikF@users.noreply.github.com>
2026-04-11 00:15:15 -0400

    added the replacement of poinds adn temperature

--- Assignment4/part1.js
+++ Assignment4/part1.js
@@ -51,6 +51,9 @@ function generateStory() {
   if (document.getElementById("uk").checked) {
     const weight = `${Math.round(300 / 14)} stone`;
     const temperature = `${Math.round((94 - 32) * (5 / 9))} Celsius`;
+    newStory=newStory.replace("300 pounds",weight);
+    newStory=newStory.replace("94 Fahrenheit",temperature);
+
   }
 
   // TODO: replace "" with the correct expression
